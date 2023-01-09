9bbaefb0dc06e977f5853012465d07b5dd2813cd
hungdao9919 <hungdao9919@gmail.com>
2023-01-10 01:59:18 +0700

    Import variable, simple products

--- src/App.js
+++ src/App.js
@@ -11,9 +11,7 @@ function App() {
         version: 'wc/v3',
     });
 
-    let productData;
     let variationData = {};
-    let deleteProductItem = [];
     let dataCsv;
     let idIndex;
     let nameIndex;
@@ -53,14 +51,13 @@ function App() {
     let productItemData = {};
     let parrentData = [];
     variationData = {};
-
-    const variableImport = () => {
-        dataCsv.forEach((item, index) => {
+    const handleImport = async () => {
+        for (const item of dataCsv) {
             if (item[nameIndex] !== undefined && item[nameIndex] !== 'Name') {
                 //import từng dòng, nếu variable thì lưu lại id hoặc sku để làm cha, import xong variable thì lấy lại id đã import lưu vào biến,
                 // khi import variation thì check id parent ở trong object parent lớn, nếu trùng thì trả lại id sản phẩm đã import trong woo, import attribute
                 //import từng dòng
-                if (item[typeIndex] === 'variable') {
+                if (item[typeIndex] === 'variable' || item[typeIndex] === 'simple') {
                     let imageUrls = item[imageIndex].split(',' || ' ' || ', ');
                     //xử lý đưa image url vô arr
                     imageUrls.forEach((imageUrl) => {
@@ -86,104 +83,210 @@ function App() {
                         },
                     ];
 
+                    const attribute1Values = item[attribute1ValueIndex].split(',');
+                    const attribute2Values = item[attribute2ValueIndex].split(',');
+                    const attribute3Values = item[attribute3ValueIndex].split(',');
                     productItemData.attributes = [
                         {
                             name: item[attribute1NameIndex],
-                            options: item[attribute1ValueIndex],
+                            options: attribute1Values,
                             visible: true,
                             variation: true,
+                            position: 0,
                         },
                         {
                             name: item[attribute2NameIndex],
-                            options: item[attribute2ValueIndex],
+                            options: attribute2Values,
                             visible: true,
+                            position: 0,
                             variation: true,
                         },
                         {
                             name: item[attribute3NameIndex],
-                            options: item[attribute3ValueIndex],
+                            options: attribute3Values,
+                            position: 0,
                             visible: true,
                             variation: true,
                         },
                     ];
-                    WooCommerce.post('products', productItemData)
-                        .then((response) => {
-                            console.log(response.data);
+                    productItemData.default_attributes = [
+                        {
+                            name: item[attribute1NameIndex],
+                            option: attribute1Values[0],
+                        },
+                        {
+                            name: item[attribute2NameIndex],
+                            option: attribute2Values[0],
+                        },
+                        {
+                            name: item[attribute3NameIndex],
+                            option: attribute3Values[0],
+                        },
+                    ];
 
-                            //import vô object chứa các id parent
-                            parrentData.push({
-                                csvParentId: productItemData.Itemid,
-                                csvParrentSku: productItemData.skuParrent,
-                                wooParrentId: response.data.id,
-                            });
-                            //import vô mảng để xóa sản phẩm
-                            deleteProductItem.push(response.data.id);
-                        })
-                        .catch((error) => {
-                            console.log(error.response.data);
+                    try {
+                        console.log(productItemData);
+                        const wooResult = await WooCommerce.post('products', productItemData);
+                        parrentData.push({
+                            csvParentId: productItemData.Itemid,
+                            csvParrentSku: productItemData.skuParrent,
+                            wooParrentId: wooResult.data.id,
                         });
+                        console.log(parrentData);
+                    } catch (e) {
+                        console.log(e);
+                    }
                 }
             }
-        });
-    };
-
-    const variationImport = () => {
-        dataCsv.forEach((item) => {
             if (item[typeIndex] === 'variation') {
                 variationData.regular_price = item[regular_priceIndex];
-                variationData.attributes = [{ name: item[attribute1NameIndex], option: item[attribute1ValueIndex] }];
-                console.log('đã click import variation');
-                console.log(parrentData);
-                parrentData.forEach((parrentDataItem) => {
+
+                for (const parrentDataItem of parrentData) {
                     if (
-                        parrentDataItem.csvParentId === item[parentIndex].replace(/id:|PR-/g, '') ||
-                        parrentDataItem.csvParrentSku === item[parentIndex].replace(/id:|PR-/g, '')
+                        parrentDataItem.csvParentId === item[parentIndex] ||
+                        parrentDataItem.csvParrentSku === item[parentIndex]
                     ) {
                         console.log(
                             'đã bắn request post Parrent',
                             `products/${parrentDataItem.wooParrentId}/variations`,
                         );
-                        WooCommerce.post(`products/${parrentDataItem.wooParrentId}/variations`, variationData)
-                            .then((response) => {
-                                console.log(response.data);
-                            })
-                            .catch((error) => {
-                                console.log(error.response.data);
-                            });
+                        try {
+                            console.log(variationData);
+
+                            const wooResult = await WooCommerce.post(
+                                `products/${parrentDataItem.wooParrentId}/variations`,
+                                variationData,
+                            );
+                            console.log(wooResult);
+                        } catch (e) {
+                            console.log(e);
+                        }
                     }
-                });
+                }
             }
-        });
+        }
     };
 
-    const handleDelete = () => {
-        const deleteProductData = {
-            delete: deleteProductItem,
-        };
-
-        console.log(deleteProductData);
-        WooCommerce.post('products/batch', deleteProductData)
+    const handleDeleteAll = () => {
+        WooCommerce.get('products')
             .then((response) => {
-                console.log('Thành công: đã xóa sản phẩm', response.data.delete[0].name);
+                const deleteAllProductList = [];
+                response.data.forEach((productItem) => {
+                    deleteAllProductList.push(productItem.id);
+                });
+                const dataDeleteAll = {
+                    delete: deleteAllProductList,
+                };
+                WooCommerce.post('products/batch', dataDeleteAll)
+                    .then((response) => {
+                        console.log(response.data);
+                    })
+                    .catch((error) => {
+                        console.log(error.response.data);
+                    });
             })
             .catch((error) => {
                 console.log(error.response.data);
             });
     };
+    const importMau = () => {
+        const data = {
+            name: 'Ship Your Idea',
+            type: 'variable',
+            description:
+                'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.',
+            short_description:
+                'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+            categories: [
+                {
+                    id: 9,
+                },
+                {
+                    id: 14,
+                },
+            ],
+            images: [
+                {
+                    src: 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_4_front.jpg',
+                },
+                {
+                    src: 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_4_back.jpg',
+                },
+                {
+                    src: 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_3_front.jpg',
+                },
+                {
+                    src: 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_3_back.jpg',
+                },
+            ],
+            attributes: [
+                {
+                    name: 'Color',
+                    position: 0,
+                    visible: true,
+                    variation: true,
+                    options: ['Black', 'Green'],
+                },
+                {
+                    name: 'Size',
+                    position: 0,
+                    visible: false,
+                    variation: true,
+                    options: ['S', 'M'],
+                },
+            ],
+            default_attributes: [
+                {
+                    name: 'Color',
+                    option: 'Black',
+                },
+                {
+                    name: 'Size',
+                    option: 'S',
+                },
+            ],
+        };
 
+        WooCommerce.post('products', data)
+            .then((response) => {
+                const dataVariation2 = {
+                    regular_price: '9.00',
+
+                    attributes: [
+                        {
+                            id: 9,
+                            option: 'Black',
+                        },
+                    ],
+                };
+
+                WooCommerce.post(`products/${response.data.id}/variations`, dataVariation2)
+                    .then((response) => {
+                        console.log(response.data);
+                    })
+                    .catch((error) => {
+                        console.log(error.response.data);
+                    });
+            })
+            .catch((error) => {
+                console.log(error.response.data);
+            });
+    };
     return (
         <div className="App">
             <div>
                 <CSVReader onFileLoaded={(data, fileInfo, originalFile) => handleReadingCsv(data)} />
             </div>
             <div>
-                <button onClick={variableImport}>Import Variable</button>
+                <button onClick={handleImport}>Import sản phẩm từ CSV</button>
             </div>
+
             <div>
-                <button onClick={variationImport}>Import variation</button>
+                <button onClick={handleDeleteAll}>Xóa tất cả sản phẩm</button>
             </div>
+
             <div>
-                <button onClick={handleDelete}>Xóa Sản phẩm vừa import</button>
+                <button onClick={importMau}>Import mẫu</button>
             </div>
         </div>
     );
